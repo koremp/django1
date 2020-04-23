e444672ccde91fe41d09b93d375f473447a00170
koremp <im.koremp@gmail.com>
2020-04-24 04:39:53 +0900

    Update models.py

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -4,7 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField('Author')
     publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
-    publication_date = models.DateField
+    publication_date = models.DateField()
 
     def __str__(self):
         return self.title
