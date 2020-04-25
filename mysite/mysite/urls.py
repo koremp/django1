1abf1fb10ed663f1e3712fc491a76354f4601fb6
koremp <im.koremp@gmail.com>
2020-04-25 19:12:44 +0900

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from mysite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),
     path('books/', include('books.urls')),
 ]
