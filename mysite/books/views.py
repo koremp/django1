d440c1f7b243b5cbf3a7933f5241531d47fd8af6
koremp <im.koremp@gmail.com>
2020-04-25 19:12:38 +0900

    Update views.py

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -3,6 +3,14 @@ from django.views.generic import ListView
 from django.views.generic import DetailView
 from books.models import Book, Author, Publisher
 
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['app_list'] = ['books']
+        return context
+
 class BooksModelView(TemplateView):
     template_name = 'books/index.html'
 
